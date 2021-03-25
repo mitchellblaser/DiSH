394780a97d3596fca4b1d1ccc3b9cfa2d6697b04
Mitch Blaser <mitchell.1080media@gmail.com>
2021-03-25 13:51:46 +1100

    Modified build script, added installer, bug fixes

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-nuitka3 dish.py --follow-imports --standalone
+nuitka3 dishclient.py --follow-imports --standalone
+
+echo $1;
+if [ $1 = "--install" ]
+then
+	cp dishclient.dist/ ~/.DiSH/. -r
+	sudo ln -s ~/.DiSH/dishclient.dist/dishclient /bin/dishclient
+fi
